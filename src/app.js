eb8f129f7aa1ccff1c79bdb1cf280044727e253c
David Suarez <dsuarez001@att.net>
2022-10-07 20:35:54 -0400

    code to randomly generate the suit and number

--- src/app.js
+++ src/app.js
@@ -2,3 +2,15 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+let generateRandomNumber = () => {
+  let numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+  let radomIndex = Math.floor(Math.random() * numbers.length);
+  return numbers[radomIndex];
+}
+
+let generateRandomSuit = () => {
+  let suits = ["diamond", "spade", "heart", "club"];
+  let radomIndex = Math.floor(Math.random() * suits.length);
+  return suits[radomIndex];
+}
