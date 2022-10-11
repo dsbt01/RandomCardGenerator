e853c9c4ef7824029cd0621c588d824425894174
David Suarez <dsuarez001@att.net>
2022-10-10 20:31:22 -0400

    done

--- src/app.js
+++ src/app.js
@@ -1,21 +1,17 @@
 window.onload = () => {
   //write your code here
-  document.querySelector('.card').classList.add('heart');
-
-  setTimeout(() => {
-    document.querySelector('.card').classList.remove('heart');
-    document.querySelector('.card').classList.add('spade');
-  }, 1000);
+  document.querySelector('.card').classList.add(generateRandomSuit());
+  document.querySelector('.number').innerHTML = generateRandomNumber();
 };
 
 let generateRandomNumber = () => {
   let numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
   let radomIndex = Math.floor(Math.random() * numbers.length);
   return numbers[radomIndex];
-}
+};
 
 let generateRandomSuit = () => {
   let suits = ["diamond", "spade", "heart", "club"];
   let radomIndex = Math.floor(Math.random() * suits.length);
   return suits[radomIndex];
-}
+};
