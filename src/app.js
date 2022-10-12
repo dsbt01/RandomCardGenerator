55bbf0ee729e124b9a7e5c98acc908b0644cc2c1
David Suarez <dsuarez001@att.net>
2022-10-12 19:02:57 -0400

    fix for css file location

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
 window.onload = () => {
   //write your code here
-  document.querySelector('.card').classList.add(generateRandomSuit());
-  document.querySelector('.number').innerHTML = generateRandomNumber();
+  document.querySelector(".card").classList.add(generateRandomSuit());
+  document.querySelector(".number").innerHTML = generateRandomNumber();
 };
 
 let generateRandomNumber = () => {
-  let numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
   let radomIndex = Math.floor(Math.random() * numbers.length);
   return numbers[radomIndex];
 };
